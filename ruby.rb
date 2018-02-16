41ec19bba9664eec3c88560c99550668c2fb722d
Ryan DeCook <ryandecook@gmail.com>
2018-02-15 22:00:09 -0600

    add basic hello world

---
+++ ruby.rb
@@ -0,0 +1,22 @@
+# Basic Ruby 'Hello World' application
+
+puts 'Hello World!'
+
+print "What's your first name? "
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What's your last name? "
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What city are you from? "
+city = gets.chomp
+city.capitalize!
+
+print "What state or province are you from? "
+state = gets.chomp
+state.upcase!
+
+# String interpolation example
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
